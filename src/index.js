e015c4fb8325078eff48bacf77e1bfd1975e5234
Alex Blackham <ablackham2000@gmail.com>
2019-02-21 14:31:24 +0000

    Made some updates and added an output. Definitely a WIP as it's ugly af

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const fs = require('fs');
 
 module.exports = {
     parse: (code, isUrl) => {
@@ -12,17 +13,50 @@ module.exports = {
 
         code = code
             .split('new String:').join('char ')
-            .split('new const String:').join('const char ')
+            .split('decl String:').join('char ')
+            .split('new const String:').join('static const char ')
             .split('new Handle:').join('Handle ')
             .split('new bool:').join('bool ')
+            .split('bool:').join('bool ')
             .split('new Float:').join('float ')
-            // .split('bool:') // Check for bools in brackets
-            // .split('bool:').join('stock bool ')
             .split('Handle:').join('Handle ')
             .split('public Action:').join('public Action ')
-            .replace(/new (.*?):/, "$1")
+            .replace(/new (.*?):/g, "$1 ")
+            .split('new ').join('int ')
+            .split('Plugin:myinfo').join('Plugin myinfo')
+            .replace(/const String:(.*?)\[]/g, "char[] $1")
         ;
 
-        console.log(code);
+        if (code.includes('#include <sourcemod>')) {
+            let publicVoids = [
+                'OnAllPluginsLoaded',
+                'OnAutoConfigsBuffered',
+                'OnClientFloodCheck',
+                'OnClientFloodResult',
+                'OnConfigsExecuted',
+                'OnGameFrame',
+                'OnLibraryAdded',
+                'OnLibraryRemoved',
+                'OnMapEnd',
+                'OnMapStart',
+                'OnPluginEnd',
+                'OnPluginPauseChange',
+                'OnPluginStart',
+                'OnServerCfg',
+            ];
+
+            publicVoids.map(v => code = code.split('public ' + v).join('public void ' + v));
+        }
+
+        // Tell it to use the new style declarations
+        code = "#pragma newdecls required\n" + code;
+
+        fs.writeFile(process.cwd() + '/output/new.sp', code, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log('The file was saved!');
+        });
     }
 };
